c9b68312bdf57377939e03ec6d6968123499d52b
mark47B <windachae47@gmail.com>
2023-07-30 22:30:49 +0300

    Last Commit

--- AccessControlService/Controllers/CredentialsChecker.cs
+++ AccessControlService/Controllers/CredentialsChecker.cs
@@ -26,34 +26,39 @@ namespace AccessControlService.Controllers
                 // If the JSON file doesn't exist, return an empty list of users.
                 return new List<User>();
             }
-            
+
             var json = System.IO.File.ReadAllText(jsonPath);
             return JsonConvert.DeserializeObject<List<User>>(json);
         }
 
         [HttpPost]
-        public IActionResult CheckAccess([FromBody] JObject requestBody)
-        {
+        public IActionResult CheckAccess([FromBody] User requestBody)
+        {   
             if (requestBody == null)
             {
                 return BadRequest("Invalid input format. The 'credentials' field is required.");
             }
 
-            if (requestBody == null || !requestBody.HasValues || !requestBody.ContainsKey("Login") || !requestBody.ContainsKey("Password"))
-            {
-                return BadRequest("Invalid input format. The 'Login' and 'Password' fields are required in 'credentials'.");
-            }
-
-            Console.WriteLine(requestBody);
-            Console.WriteLine(requestBody["Login"].ToString());
-
-            string login = requestBody["Login"].ToString();
-            string password = requestBody["Password"].ToString();
+            string login = requestBody.Login;
+            string password = requestBody.Password;
 
             bool hasUser = users.Any(u => u.Login == login && u.Password == password);
 
             var response = new { access = hasUser };
+            Response.Headers.Add("Access-Control-Allow-Origin", "*");
+            Response.Headers.Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+            Response.Headers.Add("Access-Control-Allow-Headers", "Content-Type, Authorization");
+            
             return Ok(response);
+        }
+        [HttpOptions]
+        public IActionResult Options()
+        {   
+            Response.Headers.Add("Access-Control-Allow-Origin", "*");
+            Response.Headers.Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+            Response.Headers.Add("Access-Control-Allow-Headers", "Content-Type, Authorization");
+            
+            return Ok();
         }
     }
 
